6cc97c97ee7fb3c94595ff731d2c85b2ba0546ff
smagles <lesialist@icloud.com>
2023-10-19 16:37:59 +0200

    Implemented Thymeleaf templating for time rendering and saved last valid timezone in a Cookie

--- src/main/java/time/common/Constants.java
+++ src/main/java/time/common/Constants.java
@@ -3,4 +3,5 @@ package time.common;
 public class Constants {
     public static final String TIMEZONE_PARAM = "timezone";
     public static final String UTC_TIMEZONE = "UTC";
+    public static final String LAST_TIMEZONE_COOKIE = "lastTimezone";
 }
--- src/main/java/time/servlets/TimeServlet.java
+++ src/main/java/time/servlets/TimeServlet.java
@@ -1,40 +1,81 @@
 package time.servlets;
 
-import time.common.Constants;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.FileTemplateResolver;
 import time.utils.TimezoneUtil;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.URLEncoder;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.TimeZone;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import static time.common.Constants.UTC_TIMEZONE;
+import static time.common.Constants.*;
 
 
 @WebServlet("/time")
 public class TimeServlet extends HttpServlet {
     private final TimezoneUtil timezoneUtil = new TimezoneUtil();
+    private TemplateEngine engine;
+
+    @Override
+    public void init() throws ServletException {
+        engine = new TemplateEngine();
+
+        FileTemplateResolver resolver = new FileTemplateResolver();
+        resolver.setPrefix("templates/");
+        resolver.setSuffix(".html");
+        resolver.setTemplateMode("HTML5");
+        resolver.setOrder(engine.getTemplateResolvers().size());
+        resolver.setCacheable(false);
+        engine.addTemplateResolver(resolver);
+    }
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        String timezoneParameter = request.getParameter(Constants.TIMEZONE_PARAM);
+
+        String timezoneParameter = request.getParameter(TIMEZONE_PARAM);
         TimeZone timeZone;
 
         if (timezoneParameter != null) {
-            timeZone = timezoneUtil.getTimeZoneFromRequest(timezoneParameter);
+            timezoneParameter = URLEncoder.encode(timezoneParameter, "UTF-8");
+            Cookie cookie = new Cookie(LAST_TIMEZONE_COOKIE, timezoneParameter);
+            response.addCookie(cookie);
+        }
+
+        Cookie[] cookies = request.getCookies();
+        String lastTimezone = null;
+        if (cookies != null) {
+            for (Cookie cookie : cookies) {
+                if (LAST_TIMEZONE_COOKIE.equals(cookie.getName())) {
+                    lastTimezone = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (lastTimezone != null) {
+            timeZone = timezoneUtil.getTimeZoneFromRequest(lastTimezone);
         } else {
             timeZone = TimeZone.getTimeZone(UTC_TIMEZONE);
         }
 
         String formattedDate = formatCurrentDateWithTimeZone(timeZone);
 
-        request.setAttribute("time", formattedDate);
-        request.getRequestDispatcher("/time-view.jsp").forward(request, response);
+        try (PrintWriter out = response.getWriter()) {
+            Context context = new Context();
+            context.setVariable("time", formattedDate);
+
+            engine.process("time-template", context, out);
+        }
     }
 
     private String formatCurrentDateWithTimeZone(TimeZone timeZone) {
